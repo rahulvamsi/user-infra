2fe8928516c75ce78e364c6a9731b65ddd610f77
Raghu K <raghuk.vit@gmail.com>
2022-09-22 07:49:01 +0530

    Update infra/user-infra/

--- mutable/main.tf
+++ mutable/main.tf
@@ -1,7 +1,8 @@
 module "user" {
-  source    = "./vendor/modules/app"
-  COMPONENT = var.COMPONENT
-  ENV       = var.ENV
-  INSTANCES = var.INSTANCES
-  APP_PORT  = 8080
+  source           = "./vendor/modules/app"
+  COMPONENT        = var.COMPONENT
+  ENV              = var.ENV
+  INSTANCES        = var.INSTANCES
+  APP_PORT         = 8080
+  LB_RULE_PRIORITY = 5
 }
